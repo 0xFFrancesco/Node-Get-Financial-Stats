8cb2a8c1059b589b6f8829e9eae9ff634823287c
Francesco Rizzi <rizzifrancesco@icloud.com>
2018-07-15 14:45:42 +0200

    RELATED BRANCH: master; MESSAGE: HTML generated output v1

--- src/get-financial-stats.js
+++ src/get-financial-stats.js
@@ -1,19 +1,26 @@
 //
 //IMPORTS
 //
-const fs        = require('fs');
+const fs        = require('fs-extra');
 const puppeteer = require('puppeteer');
 
 const timeRanges = [ '1Y', '5Y', '40Y' ];
-const output     = "";
+let output       = "";
 
 //
 //FUNCTIONS
 //
 async function saveSingleImage( element, imageName, config, logger ){
 	
-	const imageFullPath = config.save_to_dir + imageName + '.jpg';
+	const dest          = config.save_to_dir + config.assets_dir_name;
+	const imageFullPath = dest + imageName + '.jpg';
+	
 	logger(`Saving file: ${imageFullPath}...`);
+	
+	if ( !fs.existsSync(dest) ) {
+		fs.mkdirSync(dest);
+	}
+	
 	return await element.screenshot({path : imageFullPath});
 	
 }
@@ -49,17 +56,76 @@ async function takeImages( page, ticker, config, logger ){
 	
 }
 
-function convertToHTMLOutput( ticker, logger ){
+async function convertToHTMLOutput( page, ticker, config, logger ){
 	
 	logger(`Creating HTML for ${ticker}...`);
+	
+	let tickerTemplate = fs.readFileSync('./templates/ticker-template.html', 'utf8');
+	
+	const tickerFullName = await extractFieldValue('div[role="heading"] > div:nth-child(3)');
+	const pe             = await extractFieldValue(config.summary_selector + ' table tr:nth-child(5) td:nth-child(2)');
+	
+	const peData = config.p_e_ratio_warnings_strategy(pe);
+	
+	tickerTemplate = tickerTemplate.replace('{{{ticker-name}}}', ticker);
+	tickerTemplate = tickerTemplate.replace('{{{ticker-full-name}}}', tickerFullName);
+	
+	tickerTemplate = tickerTemplate.replace('{{{pe-color}}}', peData.color);
+	tickerTemplate = tickerTemplate.replace('{{{pe-label}}}', peData.label);
+	
+	let historyHTML = "";
+	let image;
+	
+	timeRanges.forEach(timeRange =>{
+		
+		image = `<img src="./${config.assets_dir_name}/${ticker}${timeRange}.jpg">`;
+		historyHTML += image;
+		
+		tickerTemplate = tickerTemplate.replace('{{{ticker-price-images}}}', historyHTML);
+		
+	});
+	
+	image          = `<img src="./${config.assets_dir_name}/${ticker}financial.jpg">`;
+	tickerTemplate = tickerTemplate.replace('{{{ticker-financial}}}', image);
+	
+	image          = `<img src="./${config.assets_dir_name}/${ticker}summary.jpg">`;
+	tickerTemplate = tickerTemplate.replace('{{{ticker-summary}}}', image);
+	
+	output += tickerTemplate;
+	
+}
+
+async function extractFieldValue( page, selector ){
+	
+	await page.evaluate(( selector ) =>{
+		
+		const DOMElement = document.querySelector(selector);
+		return DOMElement.innerText;
+		
+	}, selector);
 	
 }
 
 function deleteOldFiles( config ){
 	
 	logger(`Removing old files...`);
-	fs.unlinkSync(config.save_to_dir + config.file_name);
-	fs.unlinkSync(config.save_to_dir + config.assets_dir_name);
+	fs.removeSync(config.save_to_dir + config.file_name);
+	fs.removeSync(config.save_to_dir + config.assets_dir_name);
+	
+}
+
+function copyAssets( config, logger ){
+	
+	logger(`Copying assets files...`);
+	
+	const from = './templates/';
+	const to   = config.save_to_dir + config.assets_dir_name;
+	
+	[ 'base.css', 'reset.css', 'grid.css' ].forEach(file =>{
+		
+		fs.copyFileSync(from + file, to + file);
+		
+	});
 	
 }
 
@@ -67,10 +133,10 @@ function createFinalHTMLPage( config, logger ){
 	
 	logger(`Creating final HTML page...`);
 	
-	let finalPage = fs.readFileSync('./templates/base.html');
+	let finalPage = fs.readFileSync('./templates/base.html', 'utf8');
 	
-	finalPage.replace('{{{assets_dir}}}', config.assets_dir_name);
-	finalPage.replace('{{{data}}}', output);
+	finalPage = finalPage.replace('{{{assets_dir}}}', config.assets_dir_name);
+	finalPage = finalPage.replace('{{{data}}}', output);
 	
 	fs.writeFileSync(config.save_to_dir + config.file_name, finalPage);
 	
@@ -99,17 +165,19 @@ async function process( config, browser, logger ){
 	const page = await browser.newPage();
 	await page.goto(config.endpoint, {waitUntil : 'load'});
 	
+	deleteOldFiles(config, logger);
+	
 	for ( let ticker of config.tickers ) {
 		
 		logger(`Processing ${ticker}...`);
 		
 		await navigateToTicker(page, ticker, config);
 		await takeImages(page, ticker, config, logger);
-		convertToHTMLOutput(ticker, config, logger);
+		await convertToHTMLOutput(page, ticker, config, logger);
 		
 	}
 	
-	deleteOldFiles(config, logger);
+	copyAssets(config, logger);
 	createFinalHTMLPage(config, logger);
 	
 }
